3cc621b2f93b949834f1407985a0f27b7b5dd310
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-10-02 11:48:14 +0200

    Drop Python < 3.8 support

--- setup.py
+++ setup.py
@@ -45,19 +45,14 @@ setup(
     author='The Foreman Project',
     author_email='foreman-dev@googlegroups.com',
     zip_safe=False,
-    python_requires=">=2.7, <4, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
+    python_requires=">=3.8, <4",
 
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
@@ -69,8 +64,7 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs']),
 
     install_requires=[
-        'ansible >= 2.5; python_version < "3.8"',
-        'ansible-core; python_version >= "3.8"',
+        'ansible-core',
     ],
 
     extras_require={
