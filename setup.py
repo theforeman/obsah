theforeman/obsah

commit 3cc621b2f93b949834f1407985a0f27b7b5dd310

Drop Python < 3.8 support

.github/workflows/main.yml
@@ -12,28 +12,20 @@ env:
 jobs:
   build:
     runs-on: ubuntu-20.04
-    container: ${{ matrix.container }}
     strategy:
       fail-fast: false
       matrix:
         python:
-          - "3.6"
-          - "3.7"
-          - "3.8"
           - "3.9"
           - "3.10"
           - "3.11"
           - "3.12"
-        include:
-          - python: "2.7"
-            container: "python:2.7"
     steps:
       - uses: actions/checkout@v4
       - name: Set up Python
         uses: actions/setup-python@v5
         with:
           python-version: ${{ matrix.python }}
-        if: matrix.container == null
       - name: Install dependencies
         run: pip install -r requirements-test.txt
       - name: Run tests

setup.py
@@ -45,19 +45,14 @@ setup(
     author='The Foreman Project',
     author_email='foreman-dev@googlegroups.com',
     zip_safe=False,
-    python_requires=">=2.7, <4, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
+    python_requires=">=3.8, <4",
 
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
@@ -69,8 +64,7 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs']),
 
     install_requires=[
-        'ansible >= 2.5; python_version < "3.8"',
-        'ansible-core; python_version >= "3.8"',
+        'ansible-core',
     ],
 
     extras_require={

commit 93b2021d2c98f76a5650946a39ae07a1836ae103

Add Python 3.8 back
3cc621b2f93b949834f1407985a0f27b7b5dd310 declared Python >= 3.8 in
setup.py but then removed 3.8 from the test matrix. This aligns
everything again.

Fixes: 3cc621b2f93b ("Drop Python < 3.8 support")

.github/workflows/main.yml
@@ -16,6 +16,7 @@ jobs:
       fail-fast: false
       matrix:
         python:
+          - "3.8"
           - "3.9"
           - "3.10"
           - "3.11"

setup.py
@@ -53,6 +53,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',

tox.ini
@@ -4,7 +4,7 @@
 # and then run "tox" from this directory.
 
 [tox]
-envlist = py39,py310,py311,py312,py313
+envlist = py38,py39,py310,py311,py312,py313
 
 [testenv]
 deps =
@@ -29,6 +29,7 @@ select = E,W,F
 
 [gh-actions]
 python =
+  3.8: py38
   3.9: py39
   3.10: py310
   3.11: py311