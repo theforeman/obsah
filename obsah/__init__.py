theforeman/obsah

commit c13603d63238d449ae23b1e7a4172f10f6bb6bde

Replace pkg_resources with importlib

obsah/__init__.py
@@ -15,9 +15,9 @@ import os
 import sys
 from collections import namedtuple
 from functools import total_ordering
+from importlib import resources
 
 import yaml
-import pkg_resources
 
 try:
     import argcomplete
@@ -207,12 +207,8 @@ class ApplicationConfig(object):
         """
         path = os.environ.get('OBSAH_DATA')
         if path is None:
-            path = pkg_resources.resource_filename(__name__, 'data')
-            if not os.path.isabs(path):
-                # this is essentially a workaround for
-                # https://github.com/pytest-dev/pytest-xdist/issues/414
-                distribution = pkg_resources.get_distribution('obsah')
-                path = os.path.join(distribution.location, path)
+            # TODO: deprecated in 3.11
+            path = resources.path(__name__, 'data')
 
         return path
 

docs/source/conf.py
@@ -24,10 +24,8 @@ copyright = '2018, The Foreman'
 author = 'The Foreman'
 
 try:
-    import pkg_resources
-    package = pkg_resources.require("obsah")[0]
-    version = package.version
-    release = package.version
+    from importlib import metadata
+    version = release = metadata.version('obsah')
 except:
     # The short X.Y version
     version = ''

commit 099b6cdfddc3ed049d6036b9b01b85ffe35f78ca

Correct call to resources.path
This method is a context manager and needs to be called as such.

It removes the TODO because while Python 3.11 marked it as deprecated,
Python 3.12 undeprecated it again. While cleaning up it also removes a
redundant empty line that violates pep8.

Fixes: c13603d63238 ("Replace pkg_resources with importlib")

obsah/__init__.py
@@ -178,7 +178,6 @@ class ApplicationConfig(object):
         """
         return 'metadata.obsah.yaml'
 
-
     @staticmethod
     def data_path():
         """
@@ -186,8 +185,8 @@ class ApplicationConfig(object):
         """
         path = os.environ.get('OBSAH_DATA')
         if path is None:
-            # TODO: deprecated in 3.11
-            path = resources.path(__name__, 'data')
+            with resources.path(__name__, 'data') as fspath:
+                path = fspath.as_posix()
 
         return path
 