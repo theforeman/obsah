4c10f3595dc10d4eda1163b4c6f6f54ef4b8cce4
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2025-04-30 12:12:46 +0200

    Drop license classifier in favor of parameter

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
