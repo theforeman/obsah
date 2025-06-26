theforeman/obsah

commit 63aebd096813305361a6fc59b5ea284fa2bb6f37

don't fail when obsah_reset was never set
when a playbook has no parameters at all (like pull-images in foremanctl),
`obsah_reset` is never added to the argparse Namespace and if there
*are* persisted params (from a previous run of another playbook), you
get an error:

    # foremanctl pull-images
    Traceback (most recent call last):
      File "/bin/obsah", line 33, in <module>
        sys.exit(load_entry_point('obsah==1.3.0', 'console_scripts', 'obsah')())
      File "/usr/lib/python3.9/site-packages/obsah/__init__.py", line 448, in main
        args = reset_args(application_config, args.playbook.metadata, args)
      File "/usr/lib/python3.9/site-packages/obsah/__init__.py", line 417, in reset_args
        if getattr(args, 'obsah_reset'):
    AttributeError: 'Namespace' object has no attribute 'obsah_reset'

Safeguard this case with a `getattr` with a `[]` default

obsah/__init__.py
@@ -414,10 +414,9 @@ def reset_args(application_config: ApplicationConfig, metadata: dict, args: argp
                     for arg in reset_values:
                         if arg in persist_params and persist_params[arg] == getattr(args, arg):
                             delattr(args, arg)
-            if args.obsah_reset:
-                for reset_arg in args.obsah_reset:
-                    with contextlib.suppress(AttributeError):
-                        delattr(args, reset_arg)
+            for reset_arg in getattr(args, 'obsah_reset', []):
+                with contextlib.suppress(AttributeError):
+                    delattr(args, reset_arg)
     return args
 
 

commit 6dcd9ca946db95bfb474f4727ece9901568a8e3c

don't try to iterate over `None`
Fixes: 63aebd096813305361a6fc59b5ea284fa2bb6f37

obsah/__init__.py
@@ -414,7 +414,7 @@ def reset_args(application_config: ApplicationConfig, metadata: dict, args: argp
                     for arg in reset_values:
                         if arg in persist_params and persist_params[arg] == getattr(args, arg):
                             delattr(args, arg)
-            for reset_arg in getattr(args, 'obsah_reset', []):
+            for reset_arg in (getattr(args, 'obsah_reset', None) or []):
                 with contextlib.suppress(AttributeError):
                     delattr(args, reset_arg)
     return args