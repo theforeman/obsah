theforeman/obsah

commit b4eec07ff942eaa87b28259fd4d9720e76317f83

Rotate the log file
Co-authored-by: Evgeni Golov <evgeni@golov.de>

obsah/__init__.py
@@ -9,6 +9,7 @@ from __future__ import print_function
 
 import argparse
 import contextlib
+import datetime
 import glob
 import json
 import os
@@ -325,6 +326,12 @@ class ApplicationConfig(object):
         """
         return os.path.join(cls.state_path(), 'parameters.yaml')
 
+    @classmethod
+    def log_path(cls):
+        """
+        Return the log path.
+        """
+        return os.environ.get('ANSIBLE_LOG_PATH')
 
 class ObsahArgumentParser(argparse.ArgumentParser):
     def __init__(self, *args, **kwargs):
@@ -503,6 +510,15 @@ def reset_args(application_config: ApplicationConfig, metadata: dict, args: argp
     return args
 
 
+def rotate_log(log_path: str):
+    if log_path is not None and os.path.exists(log_path):
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        if log_path.endswith('.log'):
+            backup_path = f"{log_path}[:-4].{timestamp}.log"
+        else:
+            backup_path = f"{log_path}.{timestamp}"
+        os.rename(log_path, backup_path)
+
 def main(cliargs=None, application_config=ApplicationConfig):  # pylint: disable=R0914
     """
     Main command
@@ -511,6 +527,8 @@ def main(cliargs=None, application_config=ApplicationConfig):  # pylint: disable
 
     if os.path.exists(cfg_path):
         os.environ["ANSIBLE_CONFIG"] = cfg_path
+
+    rotate_log(application_config.log_path())
 
     # this needs to be global, as otherwise PlaybookCLI fails
     # to set the verbosity correctly

tests/test_log_rotate.py
@@ -1,0 +1,21 @@
+import os
+from unittest import mock
+import obsah
+
+def test_log_path_from_env():
+    test_log_path = '/custom/path.log'
+    with mock.patch.dict(os.environ, {'ANSIBLE_LOG_PATH': test_log_path}):
+        assert obsah.ApplicationConfig.log_path() == test_log_path
+
+def test_rotate_log(tmp_path):
+    dir = tmp_path / 'logs'
+    log_file = dir / 'test.log'
+
+    dir.mkdir()
+    log_file.touch()
+
+    assert log_file.exists()
+
+    obsah.rotate_log(str(log_file))
+    assert len(os.listdir(dir)) == 1 # only the backup file should exist
+    assert not log_file.exists()

commit ffc65ba6479ac967584e9734cbf0f9be168a6c8e

fix logpath formatting
Fixes: b4eec07ff942eaa87b28259fd4d9720e76317f83

obsah/__init__.py
@@ -514,7 +514,7 @@ def rotate_log(log_path: str):
     if log_path is not None and os.path.exists(log_path):
         timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
         if log_path.endswith('.log'):
-            backup_path = f"{log_path}[:-4].{timestamp}.log"
+            backup_path = f"{log_path[:-4]}.{timestamp}.log"
         else:
             backup_path = f"{log_path}.{timestamp}"
         os.rename(log_path, backup_path)

tests/test_log_rotate.py
@@ -17,5 +17,11 @@ def test_rotate_log(tmp_path):
     assert log_file.exists()
 
     obsah.rotate_log(str(log_file))
-    assert len(os.listdir(dir)) == 1 # only the backup file should exist
+
+    log_dir_contents = list(dir.iterdir())
+
+    assert len(log_dir_contents) == 1 # only the rotated file should exist
+    assert log_dir_contents[0].suffix == '.log'
+    assert 'test.log' not in log_dir_contents[0].name
+    assert 'test.' in log_dir_contents[0].name
     assert not log_file.exists()